121dff4f48e1e687c09100b9f0c7840dd865e6a0
nhat <nhathoang101001@gmail.com>
2023-10-19 08:43:35 +0700

    test pipeline

--- append.sh
+++ append.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # New route definition
-new_route="@app.route('/jenkins')\ndef jenkins():\n        return 'this page changed by Jenkins pipeline'\n"
+new_route="@app.route('/jenkins')\ndef jenkins():\n        return 'this web changed by Jenkins pipeline'\n"
 
 # Use sed to insert the new route at line 12 of app.py
 sed -i '12i\'"$new_route" app.py
